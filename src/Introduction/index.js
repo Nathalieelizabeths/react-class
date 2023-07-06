b59c8be154c232f7c644ddfd7abb6270994b5179
Nathalieelizabeth <elizabethsnathalie@gmail.com>
2023-07-06 07:39:31 +0300

    first commit

---
+++ src/Introduction/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './style.css'
+
+const Introduction =()=>{
+return(
+    <div>
+        <h1>What i have learnt about ReactJS</h1>
+        <p>It has reusable components</p>
+        <p>The writing format is JSK</p>
+        <p>It is fast</p>
+        <h2>Fruits</h2>
+        <img src='/images/range.jpeg' id="orange" alt="pictureorange"/>
+        <img src='/images/banane.jpg' id="banane" alt="picturebanane"/>
+        <img src='/images/mango.jpg' id="mango" alt="picturemango"/>
+        <img src='/images/apl.jpg' id="apl" alt="pictureapple"/>
+        <img src='/images/Watermelon.jpg' id="melon" alt="picturewatermelon"/>
+        <h2>Vegetables</h2>
+        <img src='/images/bellpepper.jpg' id="orange" alt="picturebell"/>
+        <img src='/images/sukuma.jpg' id="banane" alt="picturebanane"/>
+        <img src='/images/tomatoes.png' id="mango" alt="picturemango"/>
+        <img src='/images/onion.jpg' id="apl" alt="pictureapple"/>
+        <img src='/images/kabechao.jpg' id="melon" alt="picturewatermelon"/>
+        
+        
+
+    </div>
+)
+}
+
+export default Introduction;
